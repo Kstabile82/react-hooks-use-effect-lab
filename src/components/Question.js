aa5aac3fff34d273bf33c266222783940533843b
KarinaStabile <karina.stabile@gmail.com>
2021-12-12 10:47:49 -0500

    commit tree 31f3c92a8d161f650c0631cef584f5dccb6931ef

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  // add useEffect code 
+useEffect(() => {
+  const timer = setTimeout(() => {
+    setTimeRemaining(timeRemaining-1)}, 1000);
+    if (timeRemaining === 0) {
+      setTimeRemaining(10)
+      onAnswered(false)
+    }
+    return function cleanup () {
+      clearTimeout(timer);
+    } 
+  });
+
+
+// countdown timer 10 seconds/q
+// use setTimeout to run a callback fn after 1 seconds
+//   inside this, use setTimeRemaining to -1 timeRem. each second
+//   when timerem hits 0, set it back to 10 sec, call onAnswered(false)
+//cleanup fn
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
